27e7ca00fc9256c2eaad2c340bb58851ad9efb85
Michael Wells <64033124+mehtulbastro@users.noreply.github.com>
2020-08-31 13:02:04 -0400

    additional touchups

---
+++ linux/cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Clean up temp directories rm -r recursive -f is force
+rm -rf /tmp/*
+rm -rf /var/tmp/*
+
+# Clear apt cache
+apt clean -y
+
+# Clear thumbnail cache for sysadmin, instructor, and student
+rm -rf /home/sysadmin/.cache/thumbnails
+rm -rf /home/instructor/.cache/thumbnails
+rm -rf /home/student/.cache/thumbnails
+rm -rf /root/.cache/thumbnails
+
